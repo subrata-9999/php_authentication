7567e793ac75cde69bded9d0a9645f5e8cc41a38
Subrata Pramanik <subrataparam7@gmail.com>
2023-08-15 13:54:43 +0530

    second commit

--- login.php
+++ login.php
@@ -68,7 +68,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-PRrgQVJ8NNHGieOA1grGdCTIt4h21CzJs6SnWH4YMQ6G5F5+IEzOHz67L4SQaF0o" crossorigin="anonymous">
 
-    <title>php login</title>
+    <title>PHP Login</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg bg-dark bg-body-tertiary" data-bs-theme="dark">
